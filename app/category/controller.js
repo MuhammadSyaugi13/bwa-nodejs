06cc44be4bb620422822c7cacc3b64183f6d6f51
MuhammadSyaugi13 <muhammadsyaugi13@gmail.com>
2023-10-19 17:58:40 +0700

    menconfigurasi awal project

---
+++ app/category/controller.js
@@ -0,0 +1,12 @@
+module.exports={
+    index: async (req, res)=>{
+        try {
+            res.render('index', {
+                title: 'Express JS'
+            })
+        } catch (error) {
+            console.log('error controller.index : ')
+            console.log(error);
+        }
+    }
+}
